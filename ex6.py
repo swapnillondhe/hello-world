f55b617802e078a31ce44afd99db0985ba001d12
swapnillondhe <Swapnil.londhe24@hotmail.com>
2017-07-15 09:13:21 +0530

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,20 @@
+print "Mary had a little lamb"
+print "Its fleece was white as %s" % 'snow'# replaces %s with string snow
+print "." * 10 # it multiplies th '.' 10 times
+#end is not a statement but a variable
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+#combine all end variables useng '+'
+print end1 + end2 + end3 + end4 + end5 + end6,
+# here ',' attaches cheese to burger and represent continuity
+print  end7 + end8 + end9 + end10 + end11 + end12
